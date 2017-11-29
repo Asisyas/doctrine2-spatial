2d70bc9b886127a7bd3d378de3ea103d76e626c1
Your Name <you@example.com>
2017-11-29 18:01:19 +0300

    implemented MultiPolyline type for the items of the list of Geography

---
+++ lib/CrEOF/Spatial/DBAL/Types/Geography/MultiPolygonType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kost
+ * Date: 29.11.17
+ * Time: 17:59
+ */
+
+namespace CrEOF\Spatial\DBAL\Types\Geography;
+
+
+use CrEOF\Spatial\DBAL\Types\GeographyType;
+
+class MultiPolygonType extends GeographyType
+{
+
+}
---
+++ lib/CrEOF/Spatial/PHP/Types/Geography/MultiPolygon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kost
+ * Date: 29.11.17
+ * Time: 17:58
+ */
+
+namespace CrEOF\Spatial\PHP\Types\Geography;
+
+
+use CrEOF\Spatial\PHP\Types\AbstractMultiPolygon;
+
+class MultiPolygon extends AbstractMultiPolygon implements GeographyInterface
+{
+
+}
